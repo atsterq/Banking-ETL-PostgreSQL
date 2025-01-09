164cad97f4f6e5a6879cce89620bcd5eb8a9dc0f
atsterq <atsterq@gmail.com>
2025-01-09 18:16:17 +0500

    task1.2 update

--- sql_scripts/dm_procedures.sql
+++ sql_scripts/dm_procedures.sql
@@ -59,55 +59,10 @@ do $$
 declare
     v_date date := '2018-01-01';
 begin
-    -- Запускаем процедуру для оборотов по каждому дню января 2018 года
     while v_date <= '2018-01-31' loop
-        -- Запуск процедуры для оборотов
         call ds.fill_account_turnover_f(v_date);
-
-        -- Переход к следующему дню
         v_date := v_date + interval '1 day';
     end loop;
 end;
 $$;
 
-
--- заполнить баланс за 31.12.2017
-create or replace procedure ds.fill_balance_31_12_2017()
-language plpgsql
-as $$
-declare
-    a_balance_out numeric(23, 8);
-    a_balance_out_rub numeric(23, 8);
-    a_exchange_rate numeric(23, 8);
-	record record;
-begin
-    -- для 31.12.2017 заполняем остатки из DS.FT_BALANCE_F
-    for record in
-        select account_rk, balance_out, currency_rk
-        from ds.ft_balance_f
-        where on_date = '2017-12-31'
-    loop
-        -- получаем курс на 31.12.2017
-        select coalesce(reduced_cource, 1) into a_exchange_rate
-        from ds.md_exchange_rate_d
-		where '2017-12-31' between data_actual_date and data_actual_end_date
-			and currency_rk = record.currency_rk;
-
-        -- конвертируем остаток в рубли
-        a_balance_out_rub := record.balance_out * a_exchange_rate;
-
-        -- вставляем данные в витрину остатков
-        insert into dm.dm_account_balance_f (
-            on_date, account_rk, balance_out, balance_out_rub
-        ) values (
-            '2017-12-31', record.account_rk, record.balance_out, a_balance_out_rub
-        );
-    end loop;
-end;
-$$;
-
---select coalesce(reduced_cource, 1)
---        from ds.md_exchange_rate_d
---		where '2017-12-31' between data_actual_date and data_actual_end_date;
-		
-call ds.fill_balance_31_12_2017();
