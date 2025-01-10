d69739c220c08ea94477fcba185b045a8f8a8929
atsterq <atsterq@gmail.com>
2025-01-10 19:49:34 +0500

    task1.2 update. 1st procedure is done.

--- sql_scripts/dm_procedures.sql
+++ sql_scripts/dm_procedures.sql
@@ -14,37 +14,61 @@ begin
 
 	-- расчёт данных
     -- Если по счету не было проводок в дату расчету, то он не должен попадать в витрину в эту дату.
-	-- c 2018-01-01 по 2018-01-08 не было проводок? - да
 	select 
 		i_ondate as on_date,
-		account_rk, 
-		credit_amount, 
-		credit_amount * coalesce (merd.reduced_cource, 1) as credit_amount_rub,
-		debet_amount,
-		debet_amount * coalesce (merd.reduced_cource, 1) as debet_amount_rub
-	from (
+		coalesce(balance_t.c_account_rk , balance_t.d_account_rk) as account_rk 
+		, balance_t.credit_amount 
+		, balance_t.credit_amount * coalesce (merd.reduced_cource, 1) as credit_amount_rub
+		, balance_t.debet_amount
+		, balance_t.debet_amount * coalesce (merd.reduced_cource, 1) as debet_amount_rub
+	from ( -- таблица курсов за дату
 		select 
-			reduced_cource
+			reduced_cource, currency_rk 
 		from 
 			ds.md_exchange_rate_d
 		where -- актуальный курс
 			data_actual_date = i_ondate) as merd
-	right join (
+	right join ( -- таблица балансов дебета и кредита
 		select 
-		    distinct fbf.account_rk, -- идентификатор счета, по которому были проводки в дату расчету
-			fbf.currency_rk,
-			SUM(fpf.credit_amount) as credit_amount, --сумма проводок за дату расчета
-			SUM(fpf.debet_amount) as debet_amount
+			credit_t.account_rk as c_account_rk 
+			, debet_t.account_rk as d_account_rk
+			, coalesce(credit_t.currency_rk, debet_t.currency_rk) as currency_rk 
+			, credit_t.credit_amount
+			, debet_t.debet_amount
 		from
-			ds.ft_balance_f as fbf -- 114 счетов
-		join
-			ds.ft_posting_f as fpf -- с 2018-01-09
-			on fbf.account_rk = fpf.credit_account_rk -- где счет участвовал как счет по кредиту
-		where 
-			fpf.oper_date = i_ondate -- подставить переданную дату (i_ondate)
-		group by
-			fbf.account_rk, fbf.currency_rk
-		) as balance_t
+			(select -- таблица кредита
+			    distinct fbf.account_rk, -- идентификатор счета, по которому были проводки в дату расчету
+				fbf.currency_rk
+				, SUM(fpf.credit_amount) as credit_amount --сумма проводок за дату расчета
+			from
+				ds.ft_balance_f as fbf -- 114 счетов
+			join
+				ds.ft_posting_f as fpf -- с 2018-01-09
+				on fbf.account_rk = fpf.credit_account_rk -- где счет участвовал как счет по кредиту
+			where 
+				fpf.oper_date = i_ondate -- подставить переданную дату (i_ondate)
+			group by
+				fbf.account_rk, fbf.currency_rk
+			) 
+				as credit_t
+		full join ( -- таблица дебета
+			select 
+			    distinct fbf.account_rk,
+				fbf.currency_rk
+				, SUM(fpf.debet_amount) as debet_amount
+			from
+				ds.ft_balance_f as fbf 
+			join
+				ds.ft_posting_f as fpf 
+				on fbf.account_rk = fpf.debet_account_rk -- где счет участвовал как счет по debet
+			where 
+				fpf.oper_date = i_ondate 
+			group by
+				fbf.account_rk, fbf.currency_rk
+			) 
+				as debet_t	
+		on credit_t.account_rk = debet_t.account_rk)
+		as balance_t
 	on merd.currency_rk = balance_t.currency_rk;
 end;
 $$;
@@ -101,8 +125,7 @@ begin
 	
 	-- вставляем данные
 	insert into dm.account_balance_f (
-		on_date, account_rk, credit_amount, credit_amount_rub, 
-		debet_amount, debet_amount_rub)
+	on_date, account_rk, balance_out, balance_out_rub)
 
 
 end;
