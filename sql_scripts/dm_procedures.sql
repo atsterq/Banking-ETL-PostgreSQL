6eafbb707b1effc27c8707cac2229a3da8840ae3
atsterq <atsterq@gmail.com>
2025-01-10 14:31:19 +0500

    task1.2 update

--- sql_scripts/dm_procedures.sql
+++ sql_scripts/dm_procedures.sql
@@ -12,19 +12,19 @@ begin
 		on_date, account_rk, credit_amount, credit_amount_rub, 
 		debet_amount, debet_amount_rub)
 
-	-- расчёт
-    -- Если по счету не было проводок в дату расчету, то он не должен попадать в витрину в эту дату
+	-- расчёт данных
+    -- Если по счету не было проводок в дату расчету, то он не должен попадать в витрину в эту дату.
 	-- c 2018-01-01 по 2018-01-08 не было проводок? - да
 	select 
 		i_ondate as on_date,
 		account_rk, 
 		credit_amount, 
-		credit_amount * coalesce (reduced_cource, 1) as credit_amount_rub,
+		credit_amount * coalesce (merd.reduced_cource, 1) as credit_amount_rub,
 		debet_amount,
-		debet_amount * coalesce (reduced_cource, 1) as debet_amount_rub
+		debet_amount * coalesce (merd.reduced_cource, 1) as debet_amount_rub
 	from (
 		select 
-			*
+			reduced_cource
 		from 
 			ds.md_exchange_rate_d
 		where -- актуальный курс
@@ -63,61 +63,78 @@ begin
     while i_ondate <= '2018-01-31' loop
         call ds.fill_account_turnover_f(i_ondate);
 
-        -- некст день
         i_ondate := i_ondate + interval '1 day';
     end loop;
 end;
 $$;
 
 
-
-
-
-
-
-
-
-
+select * from dm.dm_account_turnover_f as datf limit 100;
+select count(1) from dm.dm_account_turnover_f as datf ;
 
 
 -----------------------------------
--- заполнить баланс за 31.12.2017
-create or replace procedure ds.fill_balance_31_12_2017()
+-- Так как остатки за день считаются на основе остатков за предыдущий день, 
+-- вам необходимо заполнить витрину DM.DM_ACCOUNT_BALANCE_F за 31.12.2017 данными из DS.FT_BALANCE_F.
+truncate dm.dm_account_balance_f ;
+-- Заполним баланс за 31.12.2017
+insert into dm.dm_account_balance_f (
+	on_date, account_rk, balance_out, balance_out_rub)
+select
+	'2017-12-31' as on_date , account_rk, balance_out
+	, balance_out * coalesce (reduced_cource, 1) as balance_out_rub
+from
+	ds.ft_balance_f as fbf 
+left join
+	ds.md_exchange_rate_d as merd
+	on merd.currency_rk = fbf.currency_rk
+		and merd.data_actual_date = '2017-12-31';
+
+	
+-- процедура заполнения витрины остатков по лицевым счетам.
+create or replace procedure ds.fill_account_balance_f(i_ondate date)
 language plpgsql
 as $$
-declare
-    a_balance_out numeric(23, 8);
-    a_balance_out_rub numeric(23, 8);
-    a_exchange_rate numeric(23, 8);
-	record record;
 begin
-    -- для 31.12.2017 заполняем остатки из DS.FT_BALANCE_F
-    for record in
-        select account_rk, balance_out, currency_rk
-        from ds.ft_balance_f
-        where on_date = '2017-12-31'
-    loop
-        -- получаем курс на 31.12.2017
-        select coalesce(reduced_cource, 1) into a_exchange_rate
-        from ds.md_exchange_rate_d
-		where '2017-12-31' between data_actual_date and data_actual_end_date
-			and currency_rk = record.currency_rk;
+    -- удалить старые данные
+    delete from dm.account_balance_f where on_date = i_ondate;
+	
+	-- вставляем данные
+	insert into dm.account_balance_f (
+		on_date, account_rk, credit_amount, credit_amount_rub, 
+		debet_amount, debet_amount_rub)
 
-        -- конвертируем остаток в рубли
-        a_balance_out_rub := record.balance_out * a_exchange_rate;
 
-        -- вставляем данные в витрину остатков
-        insert into dm.dm_account_balance_f (
-            on_date, account_rk, balance_out, balance_out_rub
-        ) values (
-            '2017-12-31', record.account_rk, record.balance_out, a_balance_out_rub
-        );
+end;
+$$;
+
+
+-- проверка за один день
+call ds.fill_account_turnover_f('2018-01-09');
+
+
+-- заполнение dm.dm_account_turnover_f за период 2018-01
+do $$
+declare
+    i_ondate date := '2018-01-01';
+begin
+    -- цикл
+    while i_ondate <= '2018-01-31' loop
+        call ds.fill_account_turnover_f(i_ondate);
+
+        i_ondate := i_ondate + interval '1 day';
     end loop;
 end;
 $$;
 
---select coalesce(reduced_cource, 1)
---        from ds.md_exchange_rate_d
---		where '2017-12-31' between data_actual_date and data_actual_end_date;
-		
-call ds.fill_balance_31_12_2017();
+
+select * from dm.dm_account_turnover_f as datf limit 100;
+select count(1) from dm.dm_account_turnover_f as datf ;
+	
+	
+	
+	
+	
+	
+	
+	
