2a73827c4504290da3bc1c75eceea4ff42f87089
atsterq <atsterq@gmail.com>
2025-01-10 23:13:41 +0500

    task1.2 update. 2nd procedure is done.

--- sql_scripts/dm_procedures.sql
+++ sql_scripts/dm_procedures.sql
@@ -1,4 +1,5 @@
 -- 1.2
+
 -- процедура заполнения витрины оборотов по лицевым счетам
 create or replace procedure ds.fill_account_turnover_f(i_ondate date)
 language plpgsql
@@ -121,29 +122,84 @@ language plpgsql
 as $$
 begin
     -- удалить старые данные
-    delete from dm.account_balance_f where on_date = i_ondate;
+    delete from dm.dm_account_balance_f where on_date = i_ondate;
 	
 	-- вставляем данные
-	insert into dm.account_balance_f (
+	insert into dm.dm_account_balance_f (
 	on_date, account_rk, balance_out, balance_out_rub)
 
-
+	-- расчёт данных
+	with balance_t as (
+	select 
+		mad.account_rk 
+		, mad.currency_rk 
+		, mad.char_type
+		, prev_day_t.balance_out as prev_balance_out
+		, prev_day_t.balance_out_rub as prev_balance_out_rub
+		, coalesce(actual_exch_t.reduced_cource, 1) as actual_cource
+	from ds.md_account_d as mad 
+	join ( -- баланс за предыдущий день
+		select
+			dabf.account_rk 
+			, dabf.balance_out 
+			, dabf.balance_out_rub 
+		from dm.dm_account_balance_f as dabf 
+		where i_ondate - interval '1 day' = dabf.on_date 
+		) as prev_day_t
+	on mad.account_rk = prev_day_t.account_rk
+	left join ( -- актуальный курс
+		select	
+			merd.currency_rk 
+			, merd.reduced_cource 
+		from ds.md_exchange_rate_d as merd 
+		where i_ondate between merd.data_actual_date and merd.data_actual_end_date
+		) as actual_exch_t
+	on mad.currency_rk = actual_exch_t.currency_rk
+	where i_ondate between mad.data_actual_date and mad.data_actual_end_date-- 112 действующих акков
+	)
+	select -- выборка для вставки
+		balance_out_t.on_date
+		, balance_out_t.account_rk
+		, balance_out_t.balance_out
+		, balance_out_t.balance_out * balance_out_t.actual_cource as balance_out_rub
+	from (
+	select 
+		i_ondate as on_date 
+		, balance_t.account_rk
+		, balance_t.actual_cource
+		, case -- расчеты по формуле
+			when balance_t.char_type = 'А' then 
+				coalesce(balance_t.prev_balance_out, 0) 
+				+ coalesce(datf.debet_amount, 0) 
+				- coalesce(datf.credit_amount, 0)
+	        when balance_t.char_type = 'П' then 
+				coalesce(balance_t.prev_balance_out, 0)
+				- coalesce(datf.debet_amount, 0) 
+				+ coalesce(datf.credit_amount, 0)
+	        end as balance_out
+	from 
+		balance_t
+	left join
+		dm.dm_account_turnover_f as datf 
+	on balance_t.account_rk = datf.account_rk and datf.on_date = i_ondate
+	) as balance_out_t;
 end;
 $$;
 
 
 -- проверка за один день
-call ds.fill_account_turnover_f('2018-01-09');
+call ds.fill_account_balance_f('2018-01-01');
+select * from dm.dm_account_balance_f as dabf where on_date = '2018-01-01' limit 100;
 
 
--- заполнение dm.dm_account_turnover_f за период 2018-01
+-- заполнение dm.fill_account_balance_f за период 2018-01
 do $$
 declare
     i_ondate date := '2018-01-01';
 begin
     -- цикл
     while i_ondate <= '2018-01-31' loop
-        call ds.fill_account_turnover_f(i_ondate);
+        call ds.fill_account_balance_f(i_ondate);
 
         i_ondate := i_ondate + interval '1 day';
     end loop;
@@ -151,13 +207,5 @@ end;
 $$;
 
 
-select * from dm.dm_account_turnover_f as datf limit 100;
-select count(1) from dm.dm_account_turnover_f as datf ;
-	
-	
-	
-	
-	
-	
-	
-	
+-- проверка
+select count(1) from dm.dm_account_balance_f ;
